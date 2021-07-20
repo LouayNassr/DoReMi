cef409c22ed25fbd6937ef1ae27deb5624394bb2
Louay Nasr <LouayNasr@users.noreply.github.com>
2021-07-20 22:08:19 +0300

    implement full_screen and adaptive_streaming in PlayerFragment

--- app/src/main/java/com/example/doremi/Fragments/PlayerFragment.java
+++ app/src/main/java/com/example/doremi/Fragments/PlayerFragment.java
@@ -2,6 +2,7 @@ package com.example.doremi.Fragments;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -9,11 +10,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.doremi.Activities.MainActivity;
 import com.example.doremi.Models.Song;
 import com.example.doremi.R;
 import com.example.doremi.ViewModels.GalleryViewModel;
@@ -24,8 +28,11 @@ import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.audio.AudioAttributes;
 import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.PlayerNotificationManager;
+import com.google.android.exoplayer2.ui.TrackSelectionDialogBuilder;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.PicassoProvider;
 
@@ -42,23 +49,53 @@ public class PlayerFragment extends Fragment {
     private GalleryViewModel viewModel;
     private PlayerNotificationManager playerNotificationManager;
 
+    ImageButton btnFullScreen, btnSettings;
+
+    boolean flag = false;// for fullscreen
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+//        binding = FragmentPlayerBinding.inflate(inflater, container, false);
         binding = FragmentPlayerBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
+
+        btnFullScreen = binding.audioPlayerView.findViewById(R.id.exo_fullscreen_button);
+        btnSettings = binding.audioPlayerView.findViewById(R.id.exo_quality_settings);
+
+        //make activity fullscreen
+        View decorView = getActivity().getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
         viewModel = new ViewModelProvider(requireParentFragment()).get(GalleryViewModel.class);
 
         final int[] position = {getArguments().getInt("position")};
 
         Activity activity = getActivity();// this line is used to create only one instance of exoplayer
-        SimpleExoPlayer player = new SimpleExoPlayer.Builder(activity).build();
+
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(getContext());
+        trackSelector.setParameters(trackSelector.getParameters());
+
+        SimpleExoPlayer player = new SimpleExoPlayer.Builder(activity).setTrackSelector(trackSelector).build();
+
+        // audioAttributes is used to add audio focus configuration
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setUsage(C.USAGE_MEDIA)
                 .setContentType(CONTENT_TYPE_MUSIC)
                 .build();
         binding.audioPlayerView.setPlayer(player);
         player.setAudioAttributes(audioAttributes, true);
+
 
 
         viewModel.getSongs().observe(getViewLifecycleOwner(), new Observer<List<Song>>() {
@@ -70,8 +107,42 @@ public class PlayerFragment extends Fragment {
                 }
                 player.setMediaItems(songsMediaItems, true);
                 player.seekTo(position[0], C.INDEX_UNSET);
+
                 player.prepare();
-                player.play();
+                player.setPlayWhenReady(true);
+
+                binding.audioPlayerView.findViewById(R.id.exo_fullscreen_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (flag) {
+                            btnFullScreen.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen));
+                            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                        } else {
+                            btnFullScreen.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen_exit));
+                            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                        }
+                        flag = !flag;
+                    }
+                });
+
+                binding.audioPlayerView.findViewById(R.id.exo_quality_settings).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
+                        if (mappedTrackInfo != null) {
+                            int rendererIndex = 0;
+                            int rendererType = mappedTrackInfo.getRendererType(rendererIndex);
+                            boolean allowAdaptiveSelections =
+                                    rendererType == C.TRACK_TYPE_VIDEO
+                                            || (rendererType == C.TRACK_TYPE_AUDIO
+                                            && mappedTrackInfo.getTypeSupport(C.TRACK_TYPE_VIDEO)
+                                            == MappingTrackSelector.MappedTrackInfo.RENDERER_SUPPORT_NO_TRACKS);
+                            TrackSelectionDialogBuilder builder = new TrackSelectionDialogBuilder(getActivity(), "select one", trackSelector, rendererIndex);
+                            builder.setAllowAdaptiveSelections(allowAdaptiveSelections);
+                            builder.build().show();
+                        }
+                    }
+                });
                 final int[] currentIndex = {player.getCurrentWindowIndex()};
                 PlayerNotificationManager.MediaDescriptionAdapter mediaDescriptionAdapter = new PlayerNotificationManager.MediaDescriptionAdapter() {
                     @Override
@@ -119,7 +190,7 @@ public class PlayerFragment extends Fragment {
         });
 
 //        MediaItem mediaItem = MediaItem.fromUri("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4");
-        return binding.getRoot();
+        return rootView;
     }
 
     @Override
